55aafe1627ce4ec25e2c7358c6e8613e8ce21ae9
LeoDog896 <LeoDog896@hotmail.com>
2020-12-25 10:42:35 -0500

    More KDocs

--- src/main/kotlin/world/cepi/shops/menuapi/MenuClicker.kt
+++ src/main/kotlin/world/cepi/shops/menuapi/MenuClicker.kt
@@ -3,4 +3,10 @@ package world.cepi.shops.menuapi
 import net.minestom.server.inventory.click.ClickType
 
 /** Represents a clicker object, containing cancellable and a lambda */
-data class MenuClicker(val consumer: (ClickType) -> Unit = { }, val toCancel: Boolean = true)
+data class MenuClicker(
+        /** The action to run when the item is clicked*/
+        val consumer: (ClickType) -> Unit = { },
+
+        /** Whether to cancel movement. */
+        val toCancel: Boolean = true
+)
