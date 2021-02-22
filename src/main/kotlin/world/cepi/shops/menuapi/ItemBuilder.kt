3e100f2f81842e1cc167463206aead230b1d48f6
MrGeoTech <themrgeotech@gmail.com>
2021-02-21 22:20:28 -0600

    Adds useful constructor description

--- src/main/kotlin/world/cepi/shops/menuapi/ItemBuilder.kt
+++ src/main/kotlin/world/cepi/shops/menuapi/ItemBuilder.kt
@@ -4,6 +4,13 @@ import net.minestom.server.item.ItemStack
 import net.minestom.server.item.Material
 import world.cepi.kstom.asRich
 
+/**
+ * Makes a new [ItemStack] from the given parameters.
+ *
+ * @param material The [Material] that the [ItemStack] will be.
+ * @param name The displayed name that the [ItemStack] will have. Default = [Material] name
+ * @param amount The amount of items in the [ItemStack]. Default = 1
+ */
 class ItemBuilder(val material: Material, val name: String = material.name, val amount: Byte = 1) {
     fun asItem(): ItemStack {
         val itemStack = ItemStack(material, amount)
